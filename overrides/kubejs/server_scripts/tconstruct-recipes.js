52970fa572814b2cb4b006b5082d7b5e0cacce5e
Humberto Navarro <humberto.navarro.dev@gmail.com>
2023-01-27 11:34:28 -0800

    remove xp obi recipes

--- overrides/kubejs/server_scripts/tconstruct-recipes.js
+++ overrides/kubejs/server_scripts/tconstruct-recipes.js
@@ -2,7 +2,6 @@
 
 onEvent("tags.fluids", (event) => {
   console.info("loading tconstruct recipes");
-  event.add("forge:experience", "experienceobelisk:raw_experience");
   event.add("minecraft:lava", "tconstruct:flowing_blazing_blood");
   event.add("minecraft:lava", "tconstruct:flowing_molten_debris");
   event.add("minecraft:lava", "tconstruct:blazing_blood");
